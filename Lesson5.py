0ec1d217b695fb7cd4ac8ea81a76a61ffb065721
Aferokript <alberto.cardinale@bk.ru>
2025-12-14 20:32:19 +0300

    Update Lesson5.py

--- Lesson5.py
+++ Lesson5.py
@@ -27,62 +27,65 @@ runes = {
     'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
     ' ': ' '
 }
+def main():
+    for i in range(10):
+        strength = random.randint(3, 19)
+        power = random.randint(3, 19)
+        agility = random.randint(3, 19)
+        endurance = random.randint(3, 19)
+        intelligence = random.randint(3, 19)
+        luck = random.randint(3, 19)
 
-for i in range(10):
-    strength = random.randint(3, 19)
-    power = random.randint(3, 19)
-    agility = random.randint(3, 19)
-    endurance = random.randint(3, 19)
-    intelligence = random.randint(3, 19)
-    luck = random.randint(3, 19)
+        skill_list = ['Стремительный прыжок', 'Электрический выстрел', 'Ледяной удар', 'Стремительный удар',
+                      'Кислотный взгляд',
+                      'Тайный побег', 'Ледяной выстрел', 'Огненный заряд']
 
-    skill_list = ['Стремительный прыжок', 'Электрический выстрел', 'Ледяной удар', 'Стремительный удар',
-                  'Кислотный взгляд',
-                  'Тайный побег', 'Ледяной выстрел', 'Огненный заряд']
+        k = 3
+        skills = random.sample(skill_list, k)
 
-    k = 3
-    skills = random.sample(skill_list, k)
+        styled_skills = []
+        for skill in skills:
+            styled_skill = ''
+            for char in skill:
+                if char in runes:
+                    styled_skill += runes[char]
+                else:
+                    styled_skill += char
+            styled_skills.append(styled_skill)
 
-    styled_skills = []
-    for skill in skills:
-        styled_skill = ''
-        for char in skill:
-            if char in runes:
-                styled_skill += runes[char]
-            else:
-                styled_skill += char
-        styled_skills.append(styled_skill)
+        print("Выбранные навыки:", skills)
+        print("Стилизованные навыки:", styled_skills)
 
-    print("Выбранные навыки:", skills)
-    print("Стилизованные навыки:", styled_skills)
+        skill_dict = {
+            'skill_1': styled_skills[0],
+            'skill_2': styled_skills[1],
+            'skill_3': styled_skills[2]
+        }
 
-    skill_dict = {
-        'skill_1': styled_skills[0],
-        'skill_2': styled_skills[1],
-        'skill_3': styled_skills[2]
-    }
+        fake = Faker('ru_RU')
+        fake_name = fake.first_name_male()
+        fake_surname = fake.last_name()
+        fake_city = fake.city()
+        fake_job = fake.job()
 
-    fake = Faker('ru_RU')
-    fake_name = fake.first_name_male()
-    fake_surname = fake.last_name()
-    fake_city = fake.city()
-    fake_job = fake.job()
+        context = {
+            'first_name': fake_name,
+            'last_name': fake_surname,
+            'job': fake_job,
+            'town': fake_city,
+            'strength': strength,
+            'power': power,
+            'agility': agility,
+            'endurance': endurance,
+            'intelligence': intelligence,
+            'luck': luck,
+            'skill_1': styled_skills[0],
+            'skill_2': styled_skills[1],
+            'skill_3': styled_skills[2]
+        }
 
-    context = {
-        'first_name': fake_name,
-        'last_name': fake_surname,
-        'job': fake_job,
-        'town': fake_city,
-        'strength': strength,
-        'power': power,
-        'agility': agility,
-        'endurance': endurance,
-        'intelligence': intelligence,
-        'luck': luck,
-        'skill_1': styled_skills[0],
-        'skill_2': styled_skills[1],
-        'skill_3': styled_skills[2]
-    }
-    svg_content = file_operations.render_template('charsheet.svg', 'charsheet-0.svg', context)
-    card = open('charsheet.svg', 'w')
+        svg_content = file_operations.render_template('svg_files/charsheet.svg', f'svg_files/charsheet-{i}.svg',
+                                                      context)
 
+if __name__ == '__main__':
+    main()
